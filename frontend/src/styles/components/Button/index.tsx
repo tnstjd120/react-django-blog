13e8ff983f8e161bc57d6f1c53e6b2286fc76484
soonsung <tnstjd120@gmail.com>
2023-03-05 22:06:39 +0900

    styles: Button 컴포넌트 스타일

---
+++ frontend/src/styles/components/Button/index.tsx
@@ -0,0 +1,11 @@
+import { css } from '@emotion/react';
+
+export const buttonStyles = css`
+  font-size: 24px;
+  border: 0;
+  border-radius: 12px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 14px;
+`;
