781f26c34c46c679531108ff3fe084063e7766f1
soonsung <tnstjd120@gmail.com>
2023-03-05 22:13:43 +0900

    styles: Button Styles 수정

--- frontend/src/styles/components/Button/index.tsx
+++ frontend/src/styles/components/Button/index.tsx
@@ -1,11 +1,13 @@
 import { css } from '@emotion/react';
 
 export const buttonStyles = css`
-  font-size: 24px;
+  font-size: 16px;
   border: 0;
-  border-radius: 12px;
+  border-radius: 8px;
   display: flex;
   justify-content: center;
   align-items: center;
   padding: 10px 14px;
+  background-color: royalblue;
+  color: #fff;
 `;
