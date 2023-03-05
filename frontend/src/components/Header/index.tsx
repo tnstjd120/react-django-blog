979e32518aa56a1e766da44b94a64f94bdad5958
soonsung <tnstjd120@gmail.com>
2023-03-05 22:13:21 +0900

    feat: Button component 추가

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -2,11 +2,13 @@
 
 import React from 'react';
 import { headerStyle, logoStyle } from '../../styles/components/Header';
+import Button from '../Button/index';
 
 const Header: React.FC = () => {
   return (
     <header css={headerStyle}>
       <h1 css={logoStyle}>React - Django Blog</h1>
+      <Button />
     </header>
   );
 };
