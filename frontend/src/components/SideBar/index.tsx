533344b6a54e35020b9fa800d8643d9175eae318
soonsung <tnstjd120@gmail.com>
2023-03-05 16:29:19 +0900

    feat: Emotion -> styles

--- frontend/src/components/SideBar/index.tsx
+++ frontend/src/components/SideBar/index.tsx
@@ -2,41 +2,8 @@
 
 import React from 'react';
 import { MenuListType } from '@/utils/models/SideBar';
-import { css } from '@emotion/react';
+import { sideBarStyle } from '../../styles/components/SideBar';
 import { Link, useLocation } from 'react-router-dom';
-
-const sideBarStyle = css`
-  flex-basis: 300px;
-  flex-shrink: 0;
-  height: 100%;
-  background-color: #fff;
-  font-size: 18px;
-  border-right: 1px solid #ddd;
-
-  ul {
-    display: flex;
-    flex-direction: column;
-
-    li {
-      border-bottom: 1px solid #ddd;
-
-      &.active a {
-        color: royalblue;
-      }
-
-      a {
-        display: block;
-        height: 100%;
-        padding: 10px 20px;
-        transition: 0.3s;
-
-        &:hover {
-          color: royalblue;
-        }
-      }
-    }
-  }
-`;
 
 const menuList: MenuListType[] = [
   {
